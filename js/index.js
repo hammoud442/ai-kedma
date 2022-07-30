786f27c2498f4b29159fa71eb8edaa18e9ea2b28
hammoud442 <mhmdsami442@gmail.com>
2022-07-30 20:47:33 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.togglebtn').on('click',function () {
+        $('.tow').toggle('one');
+    });
+});
+
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(51.508742,-0.120850),
+        zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
